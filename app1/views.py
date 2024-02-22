9c2e127c4c70d51cb75634eadadd227142e2daf1
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-02-22 08:02:34 -0600

    .

--- app1/views.py
+++ app1/views.py
@@ -156,10 +156,9 @@ def registroPhoto(request):
             cod = fr.face_encodings(img)[0]
             listaCod.append(cod)
          path = 'home/bportillo/Proyecto1/web1/app1/static/app1/muestra.jpg'
-         '''
-         new_mensaje = str(matriz[4]).replace('"','')
-         new_mensaje = new_mensaje[new_mensaje.index(',')+1:]
-         nparr= np.fromstring(base64.b64decode(new_mensaje),np.uint8)
+         
+         
+         nparr= np.fromstring(base64.b64decode(matriz[4]),np.uint8)
          img = cv2.imdecode(nparr,cv2.IMREAD_COLOR)
          frame2 = cv2.resize(img,(0,0),None,0.25,0.25)
          rgb = cv2.cvtColor(frame2,cv2.COLOR_BGR2RGB)
@@ -186,9 +185,8 @@ def registroPhoto(request):
             if comp1 == indice:
                 codigoP = 1
                 marcaT = datetime.datetime.now()
-                response = {'codigoP':nombre,'marcaT':marcaT,'photo':matriz,'lista':clases}'''
+                response = {'codigoP':nombre,'marcaT':marcaT,'photo':matriz,'lista':clases}
 #                ingresoP.objects.create(codigoP=codigoP,nombreP=nombre,marcaT=marcaT)
-         response = {'photo':matriz,'lista':clases}
          return JsonResponse(response)
     else:
          response = {'codigoP':0,'marcaT':0,'photo':0,'mensaje':'None', 'fecha' : fecha_}
