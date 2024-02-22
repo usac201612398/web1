19ca3dfdd4d2063261bfc20fc8fd2636d8800d89
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-02-22 15:34:45 -0600

    .

--- app1/views.py
+++ app1/views.py
@@ -124,7 +124,7 @@ def registroPhoto(request):
     for i in range(0):
         
         total_entradas = ingresoP.objects.get(fecha = fecha_,evento = "Entrada")
-        ent = len(list(total_entradas.evento))
+#        ent = len(list(total_entradas.evento))
 #        total_salidas= ingresoP.objects.get(fecha = fecha_,evento = "Salida").evento
 #    if total_entradas ==None:
 #        total_entradas=0
@@ -199,17 +199,17 @@ def registroPhoto(request):
                 elif eventoT =="Salida":
                     saludo = "Excelente día " + nombre
 
-                response = {'codigoP':codigoE,'marcaT':marcaT,'photo':new_mensaje,'respuesta':vector, 'saludo':saludo, 'total':ent}
+                response = {'codigoP':codigoE,'marcaT':marcaT,'photo':new_mensaje,'respuesta':vector, 'saludo':saludo}
 #                ingresoP.objects.create(codigoP=codigoE,nombreP=nombre,marcaT=marcaT,fecha=fechaT,origen=origenT,evento=eventoT)
             else: 
                 nombre = "DESCONOCIDO"
                 saludo = "USUARIO NO REGISTRADO"
-                response = {'codigoP':nombre,'photo':new_mensaje,'respuesta':vector, 'saludo':saludo, 'total':ent}
+                response = {'codigoP':nombre,'photo':new_mensaje,'respuesta':vector, 'saludo':saludo}
 #         response = {'photo':vector, 'imagen':new_mensaje}
          return JsonResponse(response)
     else:
          saludo = ""
-         response = {'codigoP':0,'marcaT':0,'photo':0,'mensaje':'None', 'fecha' : fecha_, 'saludo':saludo, 'total':ent}
+         response = {'codigoP':0,'marcaT':0,'photo':0,'mensaje':'None', 'fecha' : fecha_, 'saludo':saludo}
 
     return render(request,'app1/reconocimientof.html',response)
 
