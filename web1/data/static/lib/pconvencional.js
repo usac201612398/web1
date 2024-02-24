9554ac36160b554d5171cb00cd5576df405bc419
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-02-24 11:17:29 -0600

    Se agrego vista y html

--- web1/data/static/lib/pconvencional.js
+++ web1/data/static/lib/pconvencional.js
@@ -244,3 +244,25 @@ function myFunction4() {
   }
 }
 
+function myFunction5() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("date");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
