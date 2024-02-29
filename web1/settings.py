aab6d7d07bf0e1619bbb9dfd391aede448814b62
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-02-29 13:45:02 -0600

    Se agregó auth-microsoft.

--- web1/settings.py
+++ web1/settings.py
@@ -139,7 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#LOGIN_URL = HttpResponseRedirect(reverse('login'))
+LOGIN_URL = reverse('login_page')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
