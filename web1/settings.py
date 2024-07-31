b932830b60aed8934f2b1f53e385910a23309b05
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-07-31 15:38:52 -0600

    Se actualizo rfacial template.

--- web1/settings.py
+++ web1/settings.py
@@ -155,7 +155,7 @@ LOGIN_REDIRECT_URL = '/'
 
 # URL base del servidor ADFS para cerrar sesión
 ADFS_LOGOUT_URL = 'https://sdc-iot.popoyan.com.gt/adfs/ls/?wa=wsignout1.0'
-LOGOUT_REDIRECT_URL = '/app1/homepage'
+LOGOUT_REDIRECT_URL = 'django_auth_adfs:login'
 
 
 # URL a la que ADFS debe redirigir después de cerrar sesión
