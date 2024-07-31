9fbee73cc9223981cc622548875aeb640e1973b0
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-07-31 17:31:04 -0600

    Se actualizo rfacial template.

--- web1/settings.py
+++ web1/settings.py
@@ -154,8 +154,7 @@ LOGIN_REDIRECT_URL = '/'
 
 
 # URL base del servidor ADFS para cerrar sesión
-ADFS_LOGOUT_URL = 'https://sdc-iot.popoyan.com.gt/adfs/ls/'
-LOGOUT_REDIRECT_URL = '/app1/homepage'
+ADFS_LOGOUT_URL = 'https://sdc-iot.popoyan.com.gt/app1/adfs/ls/'
 
 LANGUAGE_CODE = 'en-us'
 
