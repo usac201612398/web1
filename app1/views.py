8b059685478a63b534a254e2fc849e06a0671d35
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-01 12:06:48 -0600

    Se actualizo rfacial template.

--- app1/views.py
+++ app1/views.py
@@ -230,7 +230,7 @@ def registroPhoto(request):
                     coincidencia = coincidencia.first()  # O el método que necesites para obtener el primer objeto
                     if coincidencia.codigop == int(codigoE):
                         war = "match codigo"
-                        if fecha_ == coincidencia.fecha:
+                        if str(fecha_) == str(coincidencia.fecha):
                             war = "match fecha"
                     # Realizar operaciones con 'coincidencia'
                 else:
