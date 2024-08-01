7a2bc58d64e68c789067bfa857910bd3435ac0bf
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-01 11:11:52 -0600

    Se actualizo rfacial template.

--- app1/views.py
+++ app1/views.py
@@ -224,7 +224,7 @@ def registroPhoto(request):
             #    comp1 = indice
 
             #if comp1 == indice:
-                coindicencia = Ingresop.objects.get(codigop=str(codigoE))
+                #coindicencia = Ingresop.objects.get(codigop=str(codigoE))
 
                 #if str(codigoE) == str(coindicencia.codigop) and str(vector[0])==str(fecha_) and str(vector[1])==str(coindicencia.origen) and str(vector[2])==str(coindicencia.evento):
                 #    saludo = "El usuario " + coindicencia.nombrep + " ya registró hoy su " + coindicencia.evento + " en " + coindicencia.origen
