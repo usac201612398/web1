2f850e35581dae3d919a27c2fb8d32607f9b153d
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-01 11:15:59 -0600

    Se actualizo rfacial template.

--- app1/views.py
+++ app1/views.py
@@ -224,8 +224,9 @@ def registroPhoto(request):
             #    comp1 = indice
 
             #if comp1 == indice:
-                #coindicencia = Ingresop.objects.get(codigop=str(codigoE))
-
+                coindicencia = Ingresop.objects.get(codigop=str(codigoE))
+                lca = coindicencia.nombrep
+                
                 #if str(codigoE) == str(coindicencia.codigop) and str(vector[0])==str(fecha_) and str(vector[1])==str(coindicencia.origen) and str(vector[2])==str(coindicencia.evento):
                 #    saludo = "El usuario " + coindicencia.nombrep + " ya registró hoy su " + coindicencia.evento + " en " + coindicencia.origen
                 #    response = {'codigoP':nombre,'photo':new_mensaje, 'saludo':saludo, 'aux':vector}
@@ -243,7 +244,7 @@ def registroPhoto(request):
                 elif eventoT =="Salida":
                     saludo = "Excelente día " + nombre
 
-                response = {'codigoP':codigoE,'marcaT':marcaT,'photo':new_mensaje,'saludo':saludo,'total':total,'p':porcentaje}
+                response = {'codigoP':codigoE,'marcaT':marcaT,'photo':new_mensaje,'saludo':saludo,'total':total,'p':porcentaje, 'lca': lca}
                 Ingresop.objects.create(codigop=codigoE,nombrep=nombre,marcat=marcaT,fecha=fechaT,origen=origenT,evento=eventoT)
          if contador == 0:    
              nombre = "DESCONOCIDO"
