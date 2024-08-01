1754ceaf6d67c3c4c9d13cd3adf8af0966625695
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-01 11:41:59 -0600

    Se actualizo rfacial template.

--- app1/views.py
+++ app1/views.py
@@ -249,7 +249,7 @@ def registroPhoto(request):
                 elif eventoT =="Salida":
                     saludo = "Excelente día " + nombre
 
-                response = {'codigoP':codigoE,'marcaT':marcaT,'photo':new_mensaje,'saludo':saludo,'total':total,'p':porcentaje, 'lca': coincidencia}
+                response = {'codigoP':codigoE,'marcaT':marcaT,'photo':new_mensaje,'saludo':saludo,'total':total,'p':porcentaje, 'lca': coincidencia.codigop}
                 Ingresop.objects.create(codigop=codigoE,nombrep=nombre,marcat=marcaT,fecha=fechaT,origen=origenT,evento=eventoT)
          if contador == 0:    
              nombre = "DESCONOCIDO"
