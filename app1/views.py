e7f404c4d926b3afdc59eb4c41fbb34b280d2439
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-02 12:28:45 -0600

    Se actualizo rfacial template.

--- app1/views.py
+++ app1/views.py
@@ -227,9 +227,10 @@ def registroPhoto(request):
                 codigoE = clases[min].upper()
                 yi, xf, yf, xi = faceloc
                 yi, xf, yf, xi = yi*4, xf*4, yf*4, xi*4
-                #cola=[]
-                cola.append(codigoE)
-                bandera.append([vector[5],cola])
+                cola=[]
+                bandera = []
+                #cola.append(codigoE)
+                #bandera.append([vector[5],cola])
                 '''
                 if len(cola) == 5:
                     print(len(cola))
@@ -305,10 +306,10 @@ def registroPhoto(request):
                 #else:
             
          if contador_ == 0:
-             #bandera = []
-             
-             cola.append(False)
-             bandera.append([vector[5],cola])
+             bandera = []
+             cola = []
+             #cola.append(False)
+             #bandera.append([vector[5],cola])
              nombre = "DESCONOCIDO"
              saludo = "USUARIO NO REGISTRADO"
              response = {'codigoP':nombre,'photo':new_mensaje, 'saludo':saludo, 'aux':vector, 'NoElem': vector[5],'matriz': matriz, 'cola':cola, 'bandera':bandera}
