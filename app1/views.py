b585cf173238e797f0d2ee052729c745636e9000
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-02 10:00:52 -0600

    Se actualizo rfacial template.

--- app1/views.py
+++ app1/views.py
@@ -228,8 +228,8 @@ def registroPhoto(request):
                 #bandera.append(True)
                 yi, xf, yf, xi = faceloc
                 yi, xf, yf, xi = yi*4, xf*4, yf*4, xi*4
-                cola=[]
-                #cola.append(codigoE)
+                #cola=[]
+                cola.append(codigoE)
 
                 
                 '''
@@ -285,6 +285,7 @@ def registroPhoto(request):
                             response = {'codigoP':codigoE,'marcaT':marcaT,'photo':new_mensaje,'saludo':saludo,'total':total,'p':porcentaje, 'vector': vector[5],'matriz': matriz, 'cola':cola, 'bandera':bandera}
                             bandera=[]
                             cola = []
+                            matriz = []
 
                         return JsonResponse(response)
                         
@@ -296,9 +297,9 @@ def registroPhoto(request):
                 #else:
             
          if contador_ == 0:
-             bandera = []
+             #bandera = []
              
-             #bandera.append(False)
+             bandera.append(False)
              nombre = "DESCONOCIDO"
              saludo = "USUARIO NO REGISTRADO"
              response = {'codigoP':nombre,'photo':new_mensaje, 'saludo':saludo, 'aux':vector, 'bandera': bandera, 'vector': vector[5]}
@@ -306,11 +307,12 @@ def registroPhoto(request):
              if int(vector[5]) == 4 :
                  matriz.extend(cola)
                  matriz.extend(bandera)
-                 matriz=[]
+                 #matriz=[]
                  saludo = "Listo"
                  response = {'codigoP':nombre,'photo':new_mensaje, 'saludo':saludo, 'aux':vector, 'bandera': bandera, 'vector': vector[5],'matriz': matriz, 'cola':cola, 'bandera':bandera}
                  bandera=[]
                  cola = []
+                 matriz = []
              return JsonResponse(response)
          
          
