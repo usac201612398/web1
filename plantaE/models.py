b42bac7c9f8581f78fae4e2cfd796e6b52797151
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-09 15:07:18 -0600

    se agrego CRUD en plantaE

--- plantaE/models.py
+++ plantaE/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class salidasFruta(models.Model):
-    op_finca = [('VALLE','VALLE'),('RIO','RIO'),('CIP','CIP'),('FLE','FLE'),('PASTORIA','PASTORIA')]
+
     id = models.AutoField(primary_key=True)
-    finca = models.CharField(max_length=25,choices=op_finca)
+    finca = models.CharField(max_length=25)
     viaje = models.CharField(max_length=20)
     encargado = models.CharField(max_length=30)
     orden = models.CharField(max_length=20)
