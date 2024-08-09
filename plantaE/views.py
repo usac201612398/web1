8e3367b520a651e71892b0d634344a70acb68136
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-09 12:19:51 -0600

    se agrego CRUD en plantaE

--- plantaE/views.py
+++ plantaE/views.py
@@ -7,11 +7,11 @@ from .forms import salidasFrutaForm
 
 def article_list(request):
     salidas = salidasFruta.objects.all()
-    return render(request, 'salidasFruta/salidasFruta_list.html', {'registros': salidas})
+    return render(request, 'plantaE/salidasFruta_list.html', {'registros': salidas})
 
 def article_detail(request, pk):
     salidas = get_object_or_404(salidasFruta, pk=pk)
-    return render(request, 'salidasFruta/salidasFruta_detail.html', {'registros': salidas})
+    return render(request, 'plantaE/salidasFruta_detail.html', {'registros': salidas})
 
 def article_create(request):
     if request.method == 'POST':
@@ -21,7 +21,7 @@ def article_create(request):
             return redirect('salidasFruta_list')
     else:
         form =salidasFrutaForm()
-    return render(request, 'salidasFruta/salidasFruta_form.html', {'form': form})
+    return render(request, 'plantaE/salidasFruta_form.html', {'form': form})
 
 def article_update(request, pk):
     salidas = get_object_or_404(salidasFruta, pk=pk)
@@ -32,11 +32,11 @@ def article_update(request, pk):
             return redirect('salidasFruta_list')
     else:
         form = salidasFrutaForm(instance=salidas)
-    return render(request, 'salidasFruta/salidasFruta_form.html', {'form': form})
+    return render(request, 'plantaE/salidasFruta_form.html', {'form': form})
 
 def article_delete(request, pk):
     salidas = get_object_or_404(salidasFruta, pk=pk)
     if request.method == 'POST':
         salidas.delete()
         return redirect('salidasFruta_list')
-    return render(request, 'salidasFruta/salidasFruta_confirm_delete.html', {'article': salidas})
+    return render(request, 'plantaE/salidasFruta_confirm_delete.html', {'registros': salidas})
