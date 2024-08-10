9806f20d8f25fdbdc2d265b55cf749003d78bd25
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-10 13:53:36 -0600

    se agrego CRUD en plantaE

--- plantaE/forms.py
+++ plantaE/forms.py
@@ -2,12 +2,13 @@ from django import forms
 from .models import salidasFruta, usuariosAppFruta, datosProduccion, detallesProduccion
 
 class salidasFrutaForm(forms.ModelForm):
+    
     finca = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.all())
-    encargado = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.none())
-    correo = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.none())
-    orden = forms.ModelChoiceField(queryset=datosProduccion.objects.none())
-    cultivo = forms.ModelChoiceField(queryset=datosProduccion.objects.none())
-    variedad = forms.ModelChoiceField(queryset=detallesProduccion.objects.none())
+    encargado = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.all())
+    correo = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.all())
+    orden = forms.ModelChoiceField(queryset=datosProduccion.objects.all())
+    cultivo = forms.ModelChoiceField(queryset=datosProduccion.objects.all())
+    variedad = forms.ModelChoiceField(queryset=detallesProduccion.objects.all())
     class Meta:
     
         model = salidasFruta
