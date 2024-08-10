e855902e943ba4f1817d35c86a5af5c8f65a878e
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-10 11:53:05 -0600

    se agrego CRUD en plantaE

--- plantaE/views.py
+++ plantaE/views.py
@@ -21,7 +21,7 @@ def load_dataUsuario2(request):
     ordenSelect = request.GET.get('category_id')
     cultivo= datosProduccion.objects.filter(orden=ordenSelect,status="Abierta").values('cultivo')
     variedad = detallesProduccion.objects.filter(cultivo=list(cultivo)[0]['cultivo']).values('variedad')
-    return JsonResponse({'datos': list(cultivo),'variedad':variedad})
+    return JsonResponse({'datos': list(cultivo),'variedad':list(variedad)})
 
 def article_list(request):
     salidas = salidasFruta.objects.all()
