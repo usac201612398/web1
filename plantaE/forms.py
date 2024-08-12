b4e0349df21915028aa907cc69578341e26429ef
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-12 13:42:20 -0600

    se agrego CRUD en plantaE

--- plantaE/forms.py
+++ plantaE/forms.py
@@ -3,18 +3,8 @@ from .models import salidasFruta, usuariosAppFruta, datosProduccion, detallesPro
 
 class salidasFrutaForm(forms.ModelForm):
     
-    finca = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.all())
-    encargado = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.all())
-    correo = forms.ModelChoiceField(queryset=usuariosAppFruta.objects.all())
-    orden = forms.ModelChoiceField(queryset=datosProduccion.objects.all())
-    cultivo = forms.ModelChoiceField(queryset=datosProduccion.objects.all())
-    variedad = forms.ModelChoiceField(queryset=detallesProduccion.objects.none())
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if 'cultivo' in self.data:
-            cultivo_id = str(self.data['cultivo'])
-            self.fields['variedad'].queryset = detallesProduccion.objects.filter(cultivo=cultivo_id)
+
 
     class Meta:
     
