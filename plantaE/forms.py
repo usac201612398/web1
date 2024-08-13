b25cee178659c090a49a0249a64840842b176e1a
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-13 10:25:39 -0600

    se agrego CRUD en plantaE

--- plantaE/forms.py
+++ plantaE/forms.py
@@ -8,21 +8,36 @@ class salidasFrutaForm(forms.ModelForm):
     
         model = salidasFruta
         fields = ['correo','viaje','encargado',  'finca', 'cajas',  'orden', 'cultivo', 'variedad']
+        
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
 
             self.helper = FormHelper()
             self.helper.form_method = 'post'
-            self.helper.add_input(Submit('submit','Guardar'))
+            self.helper.add_input(Submit('submit','Guardar'))   
+
             self.helper.layout = Layout(
                 Fieldset(
-                    Div('viaje', css_class='form-group col-md-6'),
-                    Div('finca', css_class='form-group col-md-6'),
-                    Div('encargado', css_class='form-group col-md-6'),
-                    Div('correo', css_class='form-group col-md-6'),
-                    Div('orden', css_class='form-group col-md-6'),
-                    Div('cultivo', css_class='form-group col-md-6'),
-                    Div('variedad',css_class='form-group col-md-6'),
-                    Div('cajas', css_class='form-group col-md-6')
+                    'Información de Salida',
+                    Div(
+                        Div('viaje', css_class='form-group col-md-6'),
+                        Div('finca', css_class='form-group col-md-6'),
+                        css_class='row'
+                    ),
+                    Div(
+                        Div('encargado', css_class='form-group col-md-6'),
+                        Div('correo', css_class='form-group col-md-6'),
+                        css_class='row'
+                    ),
+                    Div(
+                        Div('orden', css_class='form-group col-md-6'),
+                        Div('cultivo', css_class='form-group col-md-6'),
+                        css_class='row'
+                    ),
+                    Div(
+                        Div('variedad', css_class='form-group col-md-6'),
+                        Div('cajas', css_class='form-group col-md-6'),
+                        css_class='row'
+                    )
                 )
             )
