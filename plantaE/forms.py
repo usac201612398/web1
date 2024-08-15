93f17ca924781584349981ed687abbe5f3400689
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-15 14:43:26 -0600

    Se actualizo rfacial template.

--- plantaE/forms.py
+++ plantaE/forms.py
@@ -83,7 +83,7 @@ class recepcionesForm(forms.ModelForm):
 class ccalidadForm(forms.ModelForm):
     op_status2 = [('Pendiente',''),('','Inspeccionado')]
 
-    recepcion = forms.CharField(widget=forms.TextInput(attrs={'class': 'my-input'}))
+    recepcion = forms.CharField(widget=forms.Select(attrs={'class': 'my-input'}))
     fecha = forms.DateField(widget=forms.DateInput(attrs={'type':'date','class': 'my-input'}))
     porcentaje= forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'my-input'}))  # Campo numérico
     status2 = forms.ChoiceField(choices=op_status2,widget=forms.Select(attrs={'class': 'my-input'}))
