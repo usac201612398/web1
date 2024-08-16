11fda9f7d36c7c977c708375138adab0a0960129
usac201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-08-16 11:28:28 -0600

    Se actualizo rfacial template.

--- plantaE/views.py
+++ plantaE/views.py
@@ -123,6 +123,7 @@ def ccalidad_update(request, pk):
             return redirect('ccalidad_list')
     else:
         form = ccalidadForm(instance=salidas)
+        return JsonResponse({'datos': form.llave})
     return render(request, 'plantaE/ccalidad_form_edit.html', {'form': form})
 
 def ccalidad_delete(request, pk):
