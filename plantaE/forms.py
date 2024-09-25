54d86bad62136af98c98a9075086b520c7b8acc8
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-09-25 10:50:44 -0600

    Se actualizo servidor

--- plantaE/forms.py
+++ plantaE/forms.py
@@ -47,14 +47,13 @@ class recepcionesForm(forms.ModelForm):
     cajas = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'my-input'}))  # Campo numérico
     libras = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'my-input'}))  # Campo numérico
     cultivo = forms.CharField(widget=forms.TextInput(attrs={'class': 'my-input'}))  # Campo de texto
-    variedad = forms.CharField(widget=forms.TextInput(attrs={'class': 'my-input'}))  # Campo de texto
     observaciones = forms.CharField(widget=forms.TextInput(attrs={'class': 'my-input'}))  # Campo de texto
     status = forms.ChoiceField(choices=op_status,widget=forms.Select(attrs={'class': 'my-input'}))
 
     class Meta:
     
         model = Recepciones
-        fields = ['recepcion','fecha','finca', 'cajas',  'libras', 'cultivo', 'variedad','observaciones','status']
+        fields = ['recepcion','fecha','finca', 'cajas',  'libras', 'cultivo','observaciones','status']
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
