61072492cf0366be89b300b6b51e5598b37b6b48
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-10-09 17:33:18 -0600

    Se actualizo servidor4

--- plantaE/views.py
+++ plantaE/views.py
@@ -660,12 +660,12 @@ def acumFruta_consultaValle(request):
         df_agrupado = df.groupby(['orden','estructura','variedad'], as_index=False).agg(
             total_cajas=('cajas', 'sum'),
             cultivo=('cultivo', 'first'),  # Conservar el primer correo asociado
-            fecha=('id', 'first'),
-            finca =('fecha', 'first'),
+            id=('id', 'first'),
+            fecha =('fecha', 'first'),
             finca =('finca', 'first'),
-            finca =('orden', 'first'),
-            finca =('variedad', 'first'),
-            finca =('estructura', 'first')
+            orden =('orden', 'first'),
+            variedad =('variedad', 'first'),
+            estructura =('estructura', 'first')
         )
 
         registros_finales = df_agrupado.to_dict(orient='records')
