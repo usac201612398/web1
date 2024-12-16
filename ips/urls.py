ab0c9d2d0760762db9c07d4e3f7773252b758c88
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-12-16 16:19:20 -0600

    Se actualizo servidor

--- ips/urls.py
+++ ips/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
 
     path('', views.index, name='index'),  # Página principal
     path('save_qr/', views.save_qr, name='save_qr'),  # Ruta para guardar el QR
+    path('visualizar/', views.ips_visualizar, name='ips_visualizar'),  # Ruta para guardar el QR
+    path('eliminar/', views.ips_borrar, name='ips_delete'),  # Ruta para guardar el QR
+    path('exportar_excel/', views.exportar_excel, name='ips_exportar'),  # Ruta para guardar el QR
+    
 ]
--- ips/views.py
+++ ips/views.py
@@ -1,9 +1,67 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from .models import QRCodeData
 import json
+from django.http import JsonResponse, HttpResponse
+from openpyxl import Workbook
+
+from django.shortcuts import get_object_or_404, redirect
+# Create your views here.
+import json
+import pandas as pd
+import pytz
+import datetime
 def index(request):
     return render(request, 'ips/escanerqr.html')
+
+def exportar_excel(request):
+    
+    # Crea un libro de Excel y una hoja
+    wb = Workbook()
+    ws = wb.active
+    ws.title = 'Datos'
+
+    # Obtén los datos de tu modelo
+    datos = QRCodeData.objects.all()
+
+    # Especifica la zona horaria deseada
+    zona_horaria_deseada = pytz.timezone('America/Guatemala')  # Cambia esto según sea necesario
+
+    # Agrega los encabezados
+    ws.append([field.name for field in QRCodeData._meta.fields])
+    # Agrega los datos  
+    for obj in datos:
+        row = []
+        for field in QRCodeData._meta.fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                # Convertir a la zona horaria deseada
+                if value.tzinfo is not None:
+                    value = value.astimezone(zona_horaria_deseada)
+                    value = value.replace(tzinfo=None)  # Eliminar la zona horaria para ser compatible con Excel
+            row.append(value)
+        ws.append(row)
+    
+    # Crea una respuesta HTTP que sirva el archivo Excel
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=datos.xlsx'
+
+    # Guarda el libro de Excel en la respuesta
+    wb.save(response)
+
+    return response
+
+def ips_visualizar(request):
+    salidas = QRCodeData.objects.all()
+    salidas = salidas.order_by('-created_at')
+    
+    return render(request, 'ips/listqr.html', {'registros': salidas})
+
+def ips_borrar(request, pk):
+    salidas = get_object_or_404(QRCodeData, pk=pk)
+    if request.method == 'POST':
+        salidas.delete()
+        return redirect('ips_visualizar')
+    return render(request, 'ips/escanerqr_confirm_delete.html', {'registros': salidas})
 
 def save_qr(request):
     if request.method == 'POST':
@@ -13,7 +71,7 @@ def save_qr(request):
 
             qr_data = data.get('qr_data')  # Obtener el valor del QR desde el JSON
             cantidad_=data.get('cantidad')
-            
+
             if qr_data:
                 # Guardar el QR en la base de datos
                 QRCodeData.objects.create(data=qr_data,cantidad=cantidad_),
