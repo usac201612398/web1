bdf40ae6efbde2b89a985f0bec164aff9e4a5ffb
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-12-16 15:20:27 -0600

    Se actualizo servidor

--- ips/views.py
+++ ips/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import QRCodeData
-
+import json
 def index(request):
     return render(request, 'ips/escanerqr.html')
 
 def save_qr(request):
     if request.method == 'POST':
-        qr_data = request.POST.get('qr_data')
-        if qr_data:
-            # Guardar el QR en la base de datos
-            QRCodeData.objects.create(data=qr_data)
-            return JsonResponse({'message': 'QR guardado correctamente'})
-        else:
-            return JsonResponse({'error': 'No se recibió ningún dato QR'}, status=400)
-        
+        try:
+            # Obtener los datos del cuerpo de la solicitud (en formato JSON)
+            data = json.loads(request.body)  # Cargar el JSON en un diccionario
+
+            qr_data = data.get('qr_data')  # Obtener el valor del QR desde el JSON
+            cantidad_=data.get('cantidad')
+            
+            if qr_data:
+                # Guardar el QR en la base de datos
+                QRCodeData.objects.create(data=qr_data,cantidad=cantidad_),
+                return JsonResponse({'message': 'QR guardado correctamente'})
+            else:
+                return JsonResponse({'error': 'No se recibió ningún dato QR'}, status=400)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Error al procesar el JSON'}, status=400)
+
     return JsonResponse({'error': 'Método no permitido'}, status=405)
 
