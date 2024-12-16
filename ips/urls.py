14e582027ed3569d99b4e8c107c3182679c4b9c7
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-12-16 17:12:11 -0600

    Se actualizo servidor

--- ips/urls.py
+++ ips/urls.py
@@ -8,9 +8,9 @@ app_main ="ips"
 urlpatterns = [
 
     path('', views.index, name='index'),  # Página principal
-    path('save_qr/', views.save_qr, name='save_qr'),  # Ruta para guardar el QR
-    path('visualizar/', views.ips_visualizar, name='ips_visualizar'),  # Ruta para guardar el QR
-    path('eliminar/', views.ips_borrar, name='ips_delete'),  # Ruta para guardar el QR
-    path('exportar_excel/', views.exportar_excel, name='ips_exportar'),  # Ruta para guardar el QR
-    
+    path('qr/save_qr/', views.save_qr, name='save_qr'),  # Ruta para guardar el QR
+    path('qr/visualizar/', views.ips_visualizar, name='ips_visualizar'),  # Ruta para guardar el QR
+    path('qr/<int:pk>/delete/', views.ips_borrar, name='ips_delete'),  # Ruta para guardar el QR
+    path('qr/exportar_excel/', views.exportar_excel, name='ips_exportar'),  # Ruta para guardar el QR
+
 ]
