02199f506a88fcfebcef27ad45a46383cde469c6
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-12-19 14:30:03 -0600

    Se actualizo servidor

--- ips/models.py
+++ ips/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class QRCodeData(models.Model):
-    op_color = [('-','-'),('AMARILLO','AMARILLO'),('ANARANJADO','ANARANJADO'),('ROJO','ROJO')]
-    op_cosechador = [('-','-'),('VM','VM')]
+    op_color = [('','Seleccione un color'),('Amarillo','Amarillo'),('Naranja','Naranja'),('Rojo','Rojo'),('Morado','Morado'),('Verde','Verde'),('Café','Café'),('Rosado','Rosado')]
+    op_cosechador = [('','Seleccione un cosechador'),('JV','JV'),('RC','RC'),('SS','SS')('SC','SC')]
     id_escaner = models.BigAutoField(primary_key=True)
     data = models.TextField()  # Campo para almacenar los datos del QR
     cantidad = models.FloatField()
