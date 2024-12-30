0bffbd27a9f747e3981d317af1573813cb5a68a2
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-12-30 15:54:31 -0600

    Se actualizo servidor

--- ips/views.py
+++ ips/views.py
@@ -87,9 +87,9 @@ def save_qr(request):
 
     return JsonResponse({'error': 'Método no permitido'}, status=405)
 
-def actualizar_registro(request, qr_data):
+def actualizar_registro(request, pk):
     # Obtener el registro a partir del qr_data
-    registro = get_object_or_404(QRCodeData, qr_data=qr_data)
+    registro = get_object_or_404(QRCodeData, qr_data=pk)
     
     if request.method == "POST":
         form = RegistroQRForm(request.POST, instance=registro)
