09f5706559cdb0c121ea3316260c652fc2331077
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2024-12-30 16:38:22 -0600

    Se actualizo servidor

--- ips/forms.py
+++ ips/forms.py
@@ -3,6 +3,10 @@ from django import forms
 from .models import QRCodeData
 
 class RegistroQRForm(forms.ModelForm):
+    op_status = [('Pendiente',''),('Cerrado','Cerrado')]
+    status = forms.ChoiceField(choices=op_status,widget=forms.Select(attrs={'class': 'my-input'}))
+
     class Meta:
         model = QRCodeData
+        
         fields = ['data','color', 'cantidad','cosechador','blossom','n_semillas','peso','status']  # Los campos que pueden ser editados
