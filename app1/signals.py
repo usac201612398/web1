13d8ade086ef049fcc428bdfd23d994ce0b33f21
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-01-08 11:36:26 -0600

    Se actualizo servidor

--- app1/signals.py
+++ app1/signals.py
@@ -12,14 +12,18 @@ def verificar_entrada_tras_salida(sender, instance, created, **kwargs):
 
         # Asegúrate de que 'instance.marcat' sea consciente (si es ingenuo)
         if instance.marcat.tzinfo is None:  # Si es ingenuo (sin zona horaria)
+            print("Marcat es ingenuo, convirtiendo a consciente")
             instance.marcat = timezone.make_aware(instance.marcat, timezone.get_current_timezone())
 
-        # Ahora puedes hacer la resta sin problemas
+        # Verificar si han pasado más de 30 minutos
         if ahora - instance.marcat > timezone.timedelta(minutes=30):
+            print(f"Han pasado más de 30 minutos desde la salida de {instance.nombrep}")
             # Verificar si es antes de las 5 PM
             if ahora.hour < 17:
+                print("Es antes de las 5 PM")
                 # Verificar si no se ha registrado una entrada después de la salida
                 if not Ingresop.objects.filter(codigop=instance.codigop, evento='Entrada', marcat__gt=instance.marcat).exists():
+                    print(f"No se ha registrado una entrada después de la salida de {instance.nombrep}")
                     # Enviar correo electrónico
                     send_mail(
                         'Alerta: Entrada no registrada',
@@ -27,6 +31,8 @@ def verificar_entrada_tras_salida(sender, instance, created, **kwargs):
                         'brandrenz99@gmail.com.gt',  # Tu dirección de correo
                         ['brandon.portillo@popoyan.com.gt'],  # Correo del usuario
                     )
+                else:
+                    print(f"Ya se ha registrado una entrada después de la salida de {instance.nombrep}")
 
 @receiver(post_save, sender=Ingresop)
 def verificar_primera_entrada(sender, instance, created, **kwargs):
