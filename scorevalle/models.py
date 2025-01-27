806a9c555d2e80882f0f0fd07828de2a0ffc850b
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-01-27 13:38:16 -0600

    Se actualizo servidor}

--- scorevalle/models.py
+++ scorevalle/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class scorepersonal(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    finca = models.CharField(max_length=20, blank=True, null=True)
+    encargado =models.CharField(max_length=20, blank=True, null=True)
+    cuadrilla = models.CharField(max_length=20, blank=True, null=True)
+    codigop = models.BigIntegerField(blank=True, null=True)
+    nombrep = models.CharField(max_length=75, blank=True, null=True)
+    estructura = models.CharField(max_length=20, blank=True, null=True)
+    area = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.nombrep)
