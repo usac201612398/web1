c579dc6ef2efc10b35349ed025a394daae5ba1d6
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-06-13 10:45:32 -0600

    Se actualizo servidor

--- plantaE/views.py
+++ plantaE/views.py
@@ -2300,7 +2300,7 @@ def procesarinvprodcontenv2(request):
             if cajas_acumuladas >= cajas_a_enviar:
                 break
 
-    return JsonResponse({'mensaje': 'Procesado correctamente', 'registros': registros, 'palet': palet,'disponibles':cajas_disponibles, 'usadas': usadas})
+    return JsonResponse({'mensaje': 'Procesado correctamente', 'registros': registros, 'palet': palet,'totalcajas':total_cajas, 'usadas': usadas})
 
 def cargacontenedores_listv2(request):
 
