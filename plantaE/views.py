616e4513e3e0c75e90f2a93b17ec7816a65e7e84
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-06-13 15:03:22 -0600

    Se actualizo servidor

--- plantaE/views.py
+++ plantaE/views.py
@@ -2219,12 +2219,8 @@ def reporte_tabla_pivote(request):
         # Calcular rendimiento (kg/m²) por semana
         semanas = [col for col in df_merge.columns if col.startswith('20')]
         for semana in semanas:
-            if df_merge['area_total'].empty():
-                df_merge[semana] = 0
-            else:
-                df_merge[semana] = df_merge[semana] / df_merge['area_total']
-
-                df_merge[semanas] = df_merge[semanas].round(2)
+            df_merge[semana] = df_merge[semana] / df_merge['area_total']
+            df_merge[semanas] = df_merge[semanas].round(2)
 
         # Convertir resultado final a tabla HTML
         tabla_html = df_merge.to_html(
