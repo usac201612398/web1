85ab658ca2f540e601550215d270d70c40305f38
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-06-20 16:20:15 -0600

    Se actualizo servidor

--- web1/settings.py
+++ web1/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
     'django_auth_adfs.middleware.LoginRequiredMiddleware',
 ]
 
-CUSTOM_FAILED_RESPONSE_VIEW = 'dot.path.to.custom.views.login_failed'
+#CUSTOM_FAILED_RESPONSE_VIEW = 'dot.path.to.custom.views.login_failed'
 ROOT_URLCONF = 'web1.urls'
 
 TEMPLATES = [
@@ -106,6 +106,17 @@ client_secret =  'jbn8Q~dlmoXCud3hbtgbNplWmMvzA9eXJ~M-Oa4b'
 tenant_id = '2e932f25-355e-45b3-bd8b-764aaf3fd625'
 
 AUTH_ADFS = {
+    'AUDIENCE': client_id,
+    'CLIENT_ID': client_id,
+    'CLIENT_SECRET': client_secret,
+    'GROUPS_CLAIM': 'groups',   # usa groups, no roles
+    'MIRROR_GROUPS': False,     # evita que falle si no hay grupos
+    'USERNAME_CLAIM': 'upn',
+    'TENANT_ID': tenant_id,
+    'RELYING_PARTY_ID': client_id,
+}
+'''
+AUTH_ADFS = {
 
     'AUDIENCE': client_id,
     'CLIENT_ID': client_id,
@@ -117,7 +128,7 @@ AUTH_ADFS = {
     'TENANT_ID': tenant_id,
     'RELYING_PARTY_ID': client_id,
 }
-
+'''
 WSGI_APPLICATION = 'web1.wsgi.application'
 
 # Database
