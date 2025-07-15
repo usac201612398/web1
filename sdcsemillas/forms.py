09b0672bd8c7b6d15b993d023f7ba32f2d1dcfe4
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-07-15 15:33:07 -0600

    Se actualizo servidor}

--- sdcsemillas/forms.py
+++ sdcsemillas/forms.py
@@ -2,56 +2,51 @@ from django import forms
 from .models import *
 
 class lotesForm(forms.ModelForm):
-
     op_ubicacion = [('','-'),('SL','SL'),('CIP','CIP'),('Cecilio', 'Cecilio'),('Bella Vista', 'Bella Vista')]
-    op_modulo = [('','-'),('Modulo 1','Modulo 1'),('Modulo 2','Modulo 2'),('Modulo 3', 'Modulo 3'),('Modulo 4','Modulo 4'),('Modulo 5','Modulo 5'),('Modulo 6', 'Modulo 6'),('Modulo 7','Modulo 7'),('Modulo 8','Modulo 8'),('Modulo 9', 'Modulo 9'),('Modulo 10','Modulo 10'),('Modulo 11','Modulo 11')]
-    op_invernadero = [('','-'),('Invernadero 1','Invernadero 1'),('Invernadero 2','Invernadero 2'),('Invernadero 3', 'Invernadero 3'),('Invernadero 4','Invernadero 4'),('Invernadero 5','Invernadero 5'),('Invernadero 6', 'Invernadero 6'),('Invernadero 7','Invernadero 7'),('Invernadero 8','Invernadero 8'),('Invernadero 9', 'Invernadero 9'),('Invernadero 10','Invernadero 10'),('Invernadero 11','Invernadero 11')]
+    op_modulo = [('','-')] + [(f'Modulo {i}', f'Modulo {i}') for i in range(1, 12)]
+    op_invernadero = [('','-')] + [(f'Invernadero {i}', f'Invernadero {i}') for i in range(1, 12)]
     op_malla = [('','-'),('Casa Malla','Casa Malla')]
     op_cultivo = [('','-'),('Chile','Chile'),('Tomate','Tomate')]
     op_status = [('','-'),('En proceso','En proceso'),('Finalizado','Finalizado'),('Anulado','Anulado')]
     op_estructura = op_invernadero + op_modulo + op_malla
-    
     op_metodos = [('','-'),('Greenhouse','Greenhouse'),('Macrotunel','Macrotunel'),('Nethouse','Nethouse')]
 
-    fecha_pl = forms.DateField(widget=forms.DateInput(attrs={'class': 'my-input'}))
-    lote_code = forms.CharField(widget=forms.TextInput(attrs={'class': 'my-input'})) 
-    variedad_code = forms.ChoiceField(
-        choices=[],
-        widget=forms.Select(attrs={'class': 'form-control', 'id': 'id_variedad_code'})
-    )
-    apodo_variedad = forms.ChoiceField(
-        choices=[],
-        required=False,
-        widget=forms.Select(attrs={'class': 'form-control', 'id': 'id_apodo_variedad'})
-    )
-
- 
-    #variedad_code = forms.ChoiceField(widget=forms.TextInput(attrs={'class': 'my-input'})) 
-    #variedad_name = forms.CharField(widget=forms.Select(attrs={'class': 'my-input'})) 
-    #apodo_variedad = forms.ChoiceField(widget=forms.TextInput(attrs={'class': 'my-input'}))
-    cultivo = forms.ChoiceField(choices=op_cultivo,widget=forms.Select(attrs={'class': 'my-input'})) 
-    ubicacion = forms.ChoiceField(choices=op_ubicacion,widget=forms.Select(attrs={'class': 'my-input'})) 
-    estructura = forms.ChoiceField(choices=op_estructura, widget=forms.Select(attrs={'class': 'my-input'}))  
-    plantas_padre = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'my-input'}))  
-    plantas_madre = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'my-input'})) 
-    harvest_code = forms.CharField(widget=forms.TextInput(attrs={'class': 'my-input'})) 
-    status = forms.ChoiceField(choices=op_status,widget=forms.Select(attrs={'class': 'my-input'}))
-    siembra_madre = forms.DateField(widget=forms.DateInput(attrs={'class': 'my-input'}))
-    metodo_prod = forms.ChoiceField(choices=op_metodos,widget=forms.Select(attrs={'class': 'my-input'}))
-    target = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'my-input'})) 
-    surface = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'my-input'}))
-    lote_code = forms.CharField(widget=forms.TextInput(attrs={'class': 'my-input'}))  
-
     class Meta:
-    
         model = lotes
-        fields = ['fecha_pl','lote_code','variedad_code','apodo_variedad','cultivo', 'ubicacion', 'estructura', 'plantas_padre','plantas_madre','harvest_code','status','siembra_madre','metodo_prod','target','surface','observaciones']
+        fields = [
+            'fecha_pl','lote_code','variedad_code','apodo_variedad','cultivo', 
+            'ubicacion', 'estructura', 'plantas_padre','plantas_madre',
+            'harvest_code','status','siembra_madre','metodo_prod','target','surface','observaciones'
+        ]
+        widgets = {
+            'fecha_pl': forms.DateInput(attrs={'class': 'my-input'}),
+            'lote_code': forms.TextInput(attrs={'class': 'my-input'}),
+            'cultivo': forms.Select(attrs={'class': 'my-input'}),
+            'ubicacion': forms.Select(attrs={'class': 'my-input'}),
+            'estructura': forms.Select(attrs={'class': 'my-input'}),
+            'plantas_padre': forms.NumberInput(attrs={'class': 'my-input'}),
+            'plantas_madre': forms.NumberInput(attrs={'class': 'my-input'}),
+            'harvest_code': forms.TextInput(attrs={'class': 'my-input'}),
+            'status': forms.Select(attrs={'class': 'my-input'}),
+            'siembra_madre': forms.DateInput(attrs={'class': 'my-input'}),
+            'metodo_prod': forms.Select(attrs={'class': 'my-input'}),
+            'target': forms.NumberInput(attrs={'class': 'my-input'}),
+            'surface': forms.NumberInput(attrs={'class': 'my-input'}),
+            'observaciones': forms.Textarea(attrs={'class': 'my-input'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super(lotesForm, self).__init__(*args, **kwargs)
         variedades_qs = variedades.objects.all()
-        self.fields['variedad_code'].choices = [('', '-')] + [(v.variedad_code, v.variedad_code) for v in variedades_qs]
-        self.fields['apodo_variedad'].choices = [('', '-')] + [(v.apodo_variedad, v.apodo_variedad) for v in variedades_qs]
+        self.fields['variedad_code'] = forms.ChoiceField(
+            choices=[('', '-')] + [(v.variedad_code, v.variedad_code) for v in variedades_qs],
+            widget=forms.Select(attrs={'class': 'my-input'}),
+        )
+        self.fields['apodo_variedad'] = forms.ChoiceField(
+            choices=[('', '-')] + [(v.apodo_variedad, v.apodo_variedad) for v in variedades_qs],
+            widget=forms.Select(attrs={'class': 'my-input'}),
+            required=False,
+        )
 
 class variedadesForm(forms.ModelForm):
 
