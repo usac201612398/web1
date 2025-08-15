856de1820c42a3586556565d5072afa7b3b5d655
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-08-15 11:22:10 -0600

    Se cargo contenido al servidor

--- plantaE/views.py
+++ plantaE/views.py
@@ -3418,7 +3418,7 @@ def boletas_constanciatrazarexpo(request):
             fechasalcontenedor=fecha,
             contenedor=contenedor).exclude(status="Anulado")
         salcontentids = conten.values_list('registro',flat=True).distinct()
-        return JsonResponse(list(salcontentids))
+        return JsonResponse(list(conten))
     
 def boletas_constanciatraza(request):
 
