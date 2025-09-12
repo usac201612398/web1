5647414fa1e4a3fb2fd952704516849d98205725
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-09-12 15:12:16 -0600

    Actualizacion

--- sdcsemillas/views.py
+++ sdcsemillas/views.py
@@ -128,7 +128,7 @@ def lotesreporte_list(request):
         
     registros_json = json.dumps(salidas, cls=DjangoJSONEncoder)
 
-    return render(request, 'sdcsemillas/lotesreporte_list.html', {'registros': datos_combinados,'registros_json': registros_json})
+    return render(request, 'sdcsemillas/lotesreporte_list.html', {'registros': datos_combinados,'registros_json': list(registros_json)})
 
 def exportar_excel_generico(request, nombre_modelo):
     # Obtiene el modelo desde el nombre
