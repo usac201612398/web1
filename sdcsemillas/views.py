2260e03f160bd3955bdd01f9df17d63d3c379605
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-09-13 09:52:21 -0600

    Actualizacion

--- sdcsemillas/views.py
+++ sdcsemillas/views.py
@@ -50,11 +50,12 @@ def lotesreporte_list(request):
     df_cosecha = pd.DataFrame(list(cosecha.objects.values()))
     df_frutos = pd.DataFrame(list(conteofrutosplanilla.objects.values()))
     df_plantas = pd.DataFrame(list(conteoplantas.objects.values()))
-    if 'siembra_madre' in df.columns:
-        df['siembra_madre'] = pd.to_datetime(df['siembra_madre'], errors='coerce')
+    
     # 2. Merge variedades con lotes
     #df = pd.merge(df_lotes, df_variedades, how="left", left_on="variedad_code", right_on="variedad_code")
-
+    df = df_lotes
+    if 'siembra_madre' in df.columns:
+        df['siembra_madre'] = pd.to_datetime(df['siembra_madre'], errors='coerce')
     # 3. Calcular siembra según género
     df['siembra'] = df.apply(
     lambda row: row['siembra_madre'] - timedelta(days=15) if row['genero'] == 'Padre'
