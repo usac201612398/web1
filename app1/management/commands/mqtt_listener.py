b5e97bfc341d762b5116735b454df9c693dc82f4
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2025-12-23 11:51:03 -0600

    Se actualizo servidor

--- app1/management/commands/mqtt_listener.py
+++ app1/management/commands/mqtt_listener.py
@@ -4,13 +4,15 @@ import json
 import paho.mqtt.client as mqtt
 from django.core.management.base import BaseCommand
 from app1.models import SensorData
-
+import django
+import os
 MQTT_HOST = "10.111.112.4"
 MQTT_PORT = 1883
 MQTT_USER = "sdc-iot"
 MQTT_PASS = "nuevacontraseña"
 TOPIC = "esp32/temperature"
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web1.settings")
+django.setup()
 class Command(BaseCommand):
     help = "Listener MQTT que guarda datos en la base de datos"
 
