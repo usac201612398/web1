311284a00cc312ae9fcb93c0b758fa6a1ffb1a2c
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2026-01-31 11:50:54 -0600

    Se actualizo el servidor

--- plantaE/views.py
+++ plantaE/views.py
@@ -2390,7 +2390,7 @@ def supervision_list(request):
         fecha__range=(inicio_semana, fin_semana)
     ).exclude(
         status='Anulado'
-    ).order_by('-id')
+    ).order_by('-id','estructura','cultivo','zona')
 
     return render(request, 'plantaE/supervision_list.html', {'registros': salidas})
 
