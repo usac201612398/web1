b1dde5e949a5a05723f91b74a6584635dcadc461
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2026-03-14 13:01:02 -0600

    Se actualizo servidor

--- plantaE/view/home_views.py
+++ plantaE/view/home_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def plantaEhomepage(request):
-    return render(request,'plantaE/plantaE_home.html')
+    return render(request,'plantaE/home/plantaE_home.html')
 
 def plantaEhomepageger(request):
-    return render(request,'plantaE/plantaE_homeGerencial.html')
+    return render(request,'plantaE/home/plantaE_homeGerencial.html')
