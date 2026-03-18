d5819a410a826c6096a0b60a647388b7f0e623d1
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2026-03-18 10:16:16 -0600

    Se actualizo servidor

--- web1/settings.py
+++ web1/settings.py
@@ -166,15 +166,20 @@ import logging
 
 LOGGING = {
     'version': 1,
+    'disable_existing_loggers': False,
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
         },
     },
     'loggers': {
-        'django_auth_adfs': {
+        'django': {
             'handlers': ['console'],
-            'level': 'DEBUG',
+            'level': 'INFO',
+        },
+        '__main__': {
+            'handlers': ['console'],
+            'level': 'INFO',
         },
     },
 }
