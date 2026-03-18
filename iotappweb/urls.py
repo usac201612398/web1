45777a9328af15fb9dc717a97069da31101d12fb
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2026-03-18 10:39:00 -0600

    Se actualizo servidor

--- iotappweb/urls.py
+++ iotappweb/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('riegos/historial', views.historial_riegos, name='riegoshistorial'),
     path('consumo/', views.consumo_acumulado, name='consumo_acumulado'),
     path('api/histograma/', views.histograma_api, name='histograma_api'),
-    path('aranet-data/', views.aranet_data, name='aranet_data'),
-    path('aranet-stream/', views.aranet_stream, name='aranet_stream'),
-    path('aranet-live/', views.aranet_live_page, name='aranet_live')
+    path('aranet-data/', views.aranet_data, name='aranet-data'),       # POST desde el sensor
+    path('aranet-stream/', views.aranet_stream, name='aranet-stream'), # SSE stream
+    path('aranet-live/', views.aranet_live_page, name='aranet-live')  # template HTML
 ]
