3b29d9b07040234e63cd6012177b09f4c54740ac
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2026-03-19 09:33:43 -0600

    Se actualizo servidor

--- web1/settings.py
+++ web1/settings.py
@@ -229,6 +229,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_URL = 'django_auth_adfs:login'
+LOGIN_EXEMPT_URLS = [
+    r'^iotappweb/aranet-data/.*$',
+]
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 LOGIN_REDIRECT_URL = '/'
