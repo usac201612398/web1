206a62a4e052d504e710935c57a0e4b3abc3b8c6
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2026-03-26 09:27:49 -0600

    Se actualizo servidor

--- plantaE/view/__init__.py
+++ plantaE/view/__init__.py
@@ -3,6 +3,7 @@ from .recepciones_views import *
 from .actpeso_views import *
 from .itemsenvios_views import *
 from .itemsprodterm_views import *
+from .itemsprodtermaux_views import *
 from .ccalidad_views import *
 from .envioslocal_views import *
 from .contenedores_views import *
