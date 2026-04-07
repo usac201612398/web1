2a54ccf8b8d51e5481c32511428228600840921c
usac@201612398 <3075926690603@ingenieria.usac.edu.gt>
2026-04-07 12:05:13 -0600

    Se actualizo servidor

--- iotappweb/models.py
+++ iotappweb/models.py
@@ -1,6 +1,35 @@
 from django.db import models
 from django.utils import timezone
 
+class SensorDetalles(models.Model):
+    id_sensor= models.BigAutoField(primary_key=True)
+    sensor = models.CharField(max_length=100, unique = True)
+    nombrearanet = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=20)  # "peso" o "temperatura", "etc"
+    priva = models.CharField(max_length=20)  
+    estructura = models.CharField(max_length=25)  
+    finca = models.CharField(max_length=30)
+    set_point = models.FloatField(default=23)
+    status = models.CharField(max_length= 30, blank = True, null =  True)
+    
+    def __str__(self):
+        return f"{self.nombrearanet} ({self.sensor})"
+
+class SensorData(models.Model):
+
+    sensor = models.ForeignKey(
+        SensorDetalles,
+        on_delete=models.CASCADE
+    )
+    metric = models.CharField(max_length=50)
+    value = models.FloatField()
+    unit = models.CharField(max_length=20, null=True, blank=True)
+    timestamp = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sensor} - {self.metric} - {self.value}"
 
 class SensorAlert(models.Model):
     sensor = models.CharField(max_length=50)
