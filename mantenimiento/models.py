d7a7190a4c4c4ba025cfc245270af420913d8b5a
unknown <3075926690603@ingenieria.usac.edu.gt>
2026-09-04 11:35:36 -0600

    Se actualiza servidor

--- mantenimiento/models.py
+++ mantenimiento/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UsuarioMantenimiento(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="mantenimiento"
+    )
+
+    puede_acceder = models.BooleanField(
+        default=False
+    )
+
+    departamento = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    cargo = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    activo = models.BooleanField(
+        default=True
+    )
