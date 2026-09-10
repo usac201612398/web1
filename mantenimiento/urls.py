81d4232b0382a5318519d9e118c7d73d14b75524
unknown <3075926690603@ingenieria.usac.edu.gt>
2026-09-10 09:38:14 -0600

    Se actualiza servidor

--- mantenimiento/urls.py
+++ mantenimiento/urls.py
@@ -8,10 +8,10 @@ app_main ="mantenimiento"
 urlpatterns = [
 
     path("", views.index, name="tablero"),
-    
+
     path("crud/gestor/ubicacion/",views.ubicaciones_list,name='ubicaciones_list'),
     path('crud/gestor/ubicacion/new/', views.ubicaciones_create, name='ubicaciones_create'),
     path('crud/gestor/ubicacion/<int:pk>/edit/', views.ubicaciones_update, name='ubicaciones_update'),
-    path('crud/gestor/ubicacion/<int:pk>/delete/', views.ubicaciones_confirm_delete, name='ubicaciones_delete'),
+    path('crud/gestor/ubicacion/<int:pk>/delete/', views.ubicaciones_delete, name='ubicaciones_delete'),
     
 ]
