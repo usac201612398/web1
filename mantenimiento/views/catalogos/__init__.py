75dee68fbaf2655d30064e3006601434bbae5cad
unknown <3075926690603@ingenieria.usac.edu.gt>
2026-09-11 10:39:13 -0600

    Se actualiza servidor

--- mantenimiento/views/catalogos/__init__.py
+++ mantenimiento/views/catalogos/__init__.py
@@ -1,6 +1,8 @@
-from .ubicaciones import *
-from .maquinas import *
 from .centrodecosto import *
-from .personal import *
-from .listamantenimientos import *
 from .distribucionmaquinas import *
+from .listamantenimientos import *
+from .maquinas import *
+from .personal import *
+from .ubicaciones import *
+
+
