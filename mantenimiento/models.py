cfb609fe48291b53e5e6135e2ef6893c1152753d
unknown <3075926690603@ingenieria.usac.edu.gt>
2026-09-16 12:11:12 -0600

    Se agrego codigo de barras

--- mantenimiento/models.py
+++ mantenimiento/models.py
@@ -58,7 +58,7 @@ class centrodecosto(models.Model):
     ]
 
     registro = models.BigAutoField(primary_key=True)
-    centrodecosto = models.CharField(max_length=100, blank=True, null=True)
+    centrodecosto = models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
     status = models.CharField(
         max_length=15,
